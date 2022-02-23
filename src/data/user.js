40a307a6fcf6283615f710623de6883e291b271f
Jon Kaplon <jon.kaplon1@gmail.com>
2022-02-23 06:19:49 -0800

    complete

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,13 @@
+import React from "react";
+// import username from "../components/Home.js";
+// import city from "../components/Home.js";
+// import image from "../components/About.js";
+
 const username = "Liza";
 const city = "New York";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+
+export { username, city, image };
